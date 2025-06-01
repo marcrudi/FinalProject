9139317ff958985982f6be22276de0ac3d1beb6a
marcrudi <marc.rudischhauser@gmail.com>
2025-06-01 21:01:36 +0200

    css changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
-<h1>Willkommen bei F1 Race Results</h1>
+<!-- src/routes/+page.svelte -->
+<script runes>
+  // reiner statischer Content, keine Logik erforderlich
+</script>
 
-<h4>
-  Auf unserer Webapplikation können die Rennresultate, Driver-Standings und Constructors-Standings 
-  der laufenden Saison, sowie diese vergangener Saisons verfolgt werden. 
-</h4>
-<h2>Viel Spass!</h2>
+<div class="page-wrapper text-center">
+  <h1 class="display-5 mb-3">Willkommen bei F1 Race Results</h1>
+  <h4 class="mb-4">
+    Auf unserer Webapplikation können die Rennresultate, Driver-Standings
+    und Constructor-Standings aufgerufen werden.
+  </h4>
+  <h2>Viel Spass!</h2>
+</div>
